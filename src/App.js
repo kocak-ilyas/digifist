94d075d6b3338af71d965cb302e8ff9936c712d2
kocak-ilyas <kocakilyas18@gmail.com>
2022-07-27 15:21:21 +0300

    right/left arrow

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
+import React, { useContext, useState } from "react";
 import { products } from "./database/db";
+
+import { ScrollMenu, VisibilityContext } from "react-horizontal-scrolling-menu";
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 import colImage from "./assets/colImage.png";
 import "./App.css";
-import { useState } from "react";
-
 function App() {
   const [selectedColorId, setSelectedColorId] = useState(1);
+  const [selected, setSelected] = useState([]);
+  const isItemSelected = (id) => !!selected.find((el) => el === id);
+
+  const handleClick = (id) => () => {
+    const itemSelected = isItemSelected(id);
+    setSelected((currentSelected) =>
+      itemSelected ? currentSelected.filter((el) => el !== id) : currentSelected.concat(id)
+    );
+  };
+
+  const LeftArrow = () => {
+    const { isFirstItemVisible, scrollPrev } = useContext(VisibilityContext);
+    return (
+      !isFirstItemVisible && (
+        <div className='arrowButton arrowBack' onClick={() => scrollPrev()}>
+          <IoIosArrowBack size={26} style={{ margin: `4px 2px` }} />
+        </div>
+      )
+    );
+  };
+
+  const RightArrow = () => {
+    const { isLastItemVisible, scrollNext } = useContext(VisibilityContext);
+    return (
+      !isLastItemVisible && (
+        <div className='arrowButton arrowForward' onClick={() => scrollNext()}>
+          <IoIosArrowForward size={26} style={{ margin: `4px 6px` }} />
+        </div>
+      )
+    );
+  };
+
   return (
     <div className='appContainer'>
       <div className='card mb-3'>
@@ -29,67 +63,75 @@ function App() {
                 </a>
               </div>
             </div>
-            {/* <hr /> */}
             <div className='row g-0 productsCardsContainer'>
-              {products.map(({ id, productName, price, color, url }, index) => (
-                <div className='col-md-4' key={id}>
-                  <div className='card' style={{ width: "225px" }}>
-                    <img src={require(`${url}`)} className='card-img-top productsImage' alt={productName} />
-                    <div className='card-body'>
-                      <h5 className='productName'>{productName}</h5>
-                      <div className='row g-0 productsCards'>
-                        <div className='col-md-5 price'>€{price}</div>
-                        <div className='col-md-7 colorDots'>
-                          <span
-                            className={selectedColorId === 1 ? "dotSelected" : "dot"}
-                            onClick={() => setSelectedColorId(1)}
-                            style={{ backgroundColor: selectedColorId === 1 ? "white" : "#99C3CC" }}>
-                            {selectedColorId === 1 && (
-                              <span
-                                className='dotSelectedInline'
-                                onClick={() => setSelectedColorId(1)}
-                                style={{ backgroundColor: "#99C3CC" }}></span>
-                            )}
-                          </span>
-                          <span
-                            className={selectedColorId === 2 ? "dotSelected" : "dot"}
-                            onClick={() => setSelectedColorId(2)}
-                            style={{ backgroundColor: selectedColorId === 2 ? "white" : "#CC9999" }}>
-                            {selectedColorId === 2 && (
-                              <span
-                                className='dotSelectedInline'
-                                onClick={() => setSelectedColorId(2)}
-                                style={{ backgroundColor: "#CC9999" }}></span>
-                            )}
-                          </span>
-                          <span
-                            className={selectedColorId === 3 ? "dotSelected" : "dot"}
-                            onClick={() => setSelectedColorId(3)}
-                            style={{ backgroundColor: selectedColorId === 3 ? "white" : "red" }}>
-                            {selectedColorId === 3 && (
-                              <span
-                                className='dotSelectedInline'
-                                onClick={() => setSelectedColorId(3)}
-                                style={{ backgroundColor: "#CB99CC" }}></span>
-                            )}
-                          </span>
-                          <span
-                            className={selectedColorId === 4 ? "dotSelected" : "dot"}
-                            onClick={() => setSelectedColorId(4)}
-                            style={{ backgroundColor: selectedColorId === 4 ? "white" : "#A6CC99" }}>
-                            {selectedColorId === 4 && (
-                              <span
-                                className='dotSelectedInline'
-                                onClick={() => setSelectedColorId(4)}
-                                style={{ backgroundColor: "#A6CC99" }}></span>
-                            )}
-                          </span>
+              <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow} className='scrollMenu'>
+                {products.map(({ id, productName, price, color, url }, index) => (
+                  <div
+                    className='col-md-4'
+                    key={id}
+                    style={{ width: `280px` }}
+                    itemID={id}
+                    title={id}
+                    onClick={handleClick(id)}
+                    selected={isItemSelected(id)}>
+                    <div className='card' style={{ width: "225px" }}>
+                      <img src={require(`${url}`)} className='card-img-top productsImage' alt={productName} />
+                      <div className='card-body'>
+                        <h5 className='productName'>{productName}</h5>
+                        <div className='row g-0 productsCards'>
+                          <div className='col-md-5 price'>€{price}</div>
+                          <div className='col-md-7 colorDots'>
+                            <span
+                              className={selectedColorId === 1 ? "dotSelected" : "dot"}
+                              onClick={() => setSelectedColorId(1)}
+                              style={{ backgroundColor: selectedColorId === 1 ? "white" : "#99C3CC" }}>
+                              {selectedColorId === 1 && (
+                                <span
+                                  className='dotSelectedInline'
+                                  onClick={() => setSelectedColorId(1)}
+                                  style={{ backgroundColor: "#99C3CC" }}></span>
+                              )}
+                            </span>
+                            <span
+                              className={selectedColorId === 2 ? "dotSelected" : "dot"}
+                              onClick={() => setSelectedColorId(2)}
+                              style={{ backgroundColor: selectedColorId === 2 ? "white" : "#CC9999" }}>
+                              {selectedColorId === 2 && (
+                                <span
+                                  className='dotSelectedInline'
+                                  onClick={() => setSelectedColorId(2)}
+                                  style={{ backgroundColor: "#CC9999" }}></span>
+                              )}
+                            </span>
+                            <span
+                              className={selectedColorId === 3 ? "dotSelected" : "dot"}
+                              onClick={() => setSelectedColorId(3)}
+                              style={{ backgroundColor: selectedColorId === 3 ? "white" : "red" }}>
+                              {selectedColorId === 3 && (
+                                <span
+                                  className='dotSelectedInline'
+                                  onClick={() => setSelectedColorId(3)}
+                                  style={{ backgroundColor: "#CB99CC" }}></span>
+                              )}
+                            </span>
+                            <span
+                              className={selectedColorId === 4 ? "dotSelected" : "dot"}
+                              onClick={() => setSelectedColorId(4)}
+                              style={{ backgroundColor: selectedColorId === 4 ? "white" : "#A6CC99" }}>
+                              {selectedColorId === 4 && (
+                                <span
+                                  className='dotSelectedInline'
+                                  onClick={() => setSelectedColorId(4)}
+                                  style={{ backgroundColor: "#A6CC99" }}></span>
+                              )}
+                            </span>
+                          </div>
                         </div>
                       </div>
                     </div>
                   </div>
-                </div>
-              ))}
+                ))}
+              </ScrollMenu>
             </div>
           </div>
         </div>
